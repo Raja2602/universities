65196b4d10401fff4606d221b752a8b0d0600247
Raja2602 <you@example.com>
2023-03-14 15:53:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios"
+import { useState,useEffect } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import filterFactory from "react-bootstrap-table2-filter";
+import { textFilter } from "react-bootstrap-table2-filter";
 function App() {
+  const[data,setData]=useState([]);
+  const[store,setStore]=useState("");
+  const[loading,setLoading]=useState(false)
+ // useEffect(()=>{
+   // getData();
+  //},[])
+  const getData=()=>{
+    setLoading(true)
+    axios("http://universities.hipolabs.com/search?country=India").then(res=>{
+      console.log(res.data);
+      setData(res.data)
+      setLoading(false)
+      })
+  }
+
+  const columns=[{
+    dataField:"name",
+    text:"Name",
+    sort: true,
+  },
+  {
+    dataField:"country",
+    text:"Country",
+    sort: true,
+  },
+  {
+    dataField:"state-province",
+    text:"Province",
+    sort: true,
+    filter:textFilter(),
+    
+  },
+  {  
+
+    dataField:"web_pages",
+    text:"Website",
+    sort: true,
+  },
+  {
+    dataField:"alpha_two_code",
+    text:"code",
+    sort: true,
+  }
+]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <button onClick={getData} >click here to see universities</button>
+     <div className="menu">    
+     <button>admission</button>
+     <button>universities</button>
+     </div>
+     
+              { (loading==true)?
+                <div  className="text-center">
+                  <div className="spinner-border text-primary " role="status">
+                    <span className="sr-only"></span>
+                  </div>
+                </div>:null
+             }
+     <BootstrapTable  keyField="name" data={data} columns={columns} striped pagination={paginationFactory()} filter={filterFactory()} />      
     </div>
   );
 }
